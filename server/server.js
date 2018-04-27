e4842e45f945c64b7ade533c308abd70fb9fec00
Sikeat <kobsikeat@outlook.com>
2018-04-28 01:29:35 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+    
+});
+
+// Start Server
+server.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
