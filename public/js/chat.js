0ff1fa69286b91aca6dfff0a319c070445a78b9b
Sikeat <kobsikeat@outlook.com>
2018-05-02 01:06:58 +0700

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,17 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-    console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        alert(JSON.stringify(params));
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No Error');
+        }
+    });
 });
 
 socket.on('disconnect', function () {
