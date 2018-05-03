b27993e17a51e43646f4c7d1cf3d732f6e8df6ba
Sikeat <kobsikeat@outlook.com>
2018-05-03 15:37:06 +0700

    Add Users class and updateUserList event

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
+
 const publicPath = path.join(__dirname, '../public');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -21,16 +24,14 @@ io.on('connection', (socket) => {
         }
 
         socket.join(params.room);
-        // socket.leave('The Office Fans');
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
 
-        // io.emit -> io.to('The Office Fans').emit;
-        // socket.broadcast.emit -> socket.broadcast.to('The Office Fans').emit;
-        // socket,emit
-
-            // socket.emit from Admin text Welcome to chat app
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        // socket.emit from Admin text Welcome to chat app
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
         // socket.broadcast.emit from Admin text New user joined
-        socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined.'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has join.`));
         callback();
     });
 
@@ -43,7 +44,16 @@ io.on('connection', (socket) => {
     socket.on('createLocationMessage', (coords) => {
         io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
-});
+
+    socket.on('disconnect', () => {
+        var user = users.removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+        }
+    });
+}); 
 
 // Start Server
 server.listen(3000, () => {
